79f7415cf30994230b910c6db589c30bdbfbd7a0
Kishan Kumar <kishankumar22770@gmail.com>
2026-06-22 11:16:53 +0530

    Add print statements demonstrating Python features

---
+++ day3_python_basics.py
@@ -0,0 +1,4 @@
+print("Hey i am a \"good boy\"\nand this view is amazing to see it") #escape sequence
+print("This doesnt \"execute")
+print("Hey" , 6 , 7 , 89 , sep="~" ,end= "nice\n")
+print("Microsoft")
