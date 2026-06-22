ec8bf9cf6258ba3710f7a6b82db2310d730a4b52
Kishan Kumar <kishankumar22770@gmail.com>
2026-06-22 11:51:21 +0530

    Add variable and data type examples in day6.py

---
+++ day6.py
@@ -0,0 +1,21 @@
+# Variables and Data Types in Python
+a = 1234575297254
+print(a)
+Kishan = 18
+b= "Kishan"
+print(b)
+print(type(a))
+print(type(b))
+print(a ,b)
+c = True
+print(c)
+d = None
+print(d)
+print(type(c))
+print(type(d))
+e = 7.349
+print(e)
+f = complex(8, 9)
+print(f)
+print(type(e))
+print(type(f))
