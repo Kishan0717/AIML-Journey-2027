951598ba80e25ea31a31299c1971a5c3c6992c93
Kishan Kumar <kishankumar22770@gmail.com>
2026-06-22 11:48:02 +0530

    Add basic Python operations and print statements

---
+++ day4_python_basics.py
@@ -0,0 +1,18 @@
+name = "My Name is Kishan Kumar"
+print(name)
+age = 19
+print("Age:", age)
+college = "Sage University Bhopal"
+print("College:", college)
+a = 25
+b = 10
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+print("Floor Division:", a // b)
+arr = [5, 10, 15, 20]
+print("Length of array:", len(arr))
+print(10/3)
