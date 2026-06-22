a168db05866c91770fb02e42a5341318229dddab
Kishan Kumar <kishankumar22770@gmail.com>
2026-06-22 11:54:27 +0530

    Implement type conversion and user input examples
    Add various examples of type conversion and user input handling.

---
+++ day7.py
@@ -0,0 +1,55 @@
+a = "1"
+a = 1
+b = "3"
+b= 2
+print(int(a)+int(b))
+age = 20
+print("Next year your age will be " + str(age + 5))
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+# print(a + b)
+print(int(a) + int(b))
+a= int("15")
+b= int("5")
+print(a/b)
+age = input("Age: ")
+print(int(age) + 1)
+length = input("Enter length: ")
+width = input("Enter width: ")
+print("Area of rectangle : " + str(int(length) * int(width)))
+length = 45
+width = 5
+
+print(length * width)
+a = 10
+b = "10"
+
+print(a == b)
+
+a = input()
+b = input()
+
+print(a + b)
+print(int(a) + int(b))
+a = "10"
+b = 25
+print(int(a)+b)
+string = "10"
+number = 2
+string = int(string)
+print(string + number)
+# implicit type conversion
+a = 7
+print(type(a))
+b = 2.5
+print(type(b))
+c = a + b
+print(c)
+print(type(c))
+# Taking user input in python
+a = input("Enter your name: ")
+print("My name is " + a)
+x= input("Enter first number: ")
+y= input("Enter second number: ")
+print(int(x)+int(y))
